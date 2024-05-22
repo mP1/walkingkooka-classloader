2a8e7170949ed231aea828db598362f2a45a488c
Miroslav Pokorny <miroslav.pokorny@gmail.com>
2024-05-22 22:33:21 +1000

    ClassLoaderResourceProviders.jarFileWithLibs

--- src/test/java/walkingkooka/classloader/ClassLoaderResourceProvidersTest.java
+++ src/test/java/walkingkooka/classloader/ClassLoaderResourceProvidersTest.java
@@ -17,12 +17,172 @@
 
 package walkingkooka.classloader;
 
+import org.junit.jupiter.api.Test;
+import walkingkooka.Binary;
+import walkingkooka.collect.map.Maps;
 import walkingkooka.reflect.JavaVisibility;
 import walkingkooka.reflect.PublicStaticHelperTesting;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.lang.reflect.Method;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+import java.util.jar.JarEntry;
+import java.util.jar.JarInputStream;
+import java.util.jar.JarOutputStream;
 
-public final class ClassLoaderResourceProvidersTest implements PublicStaticHelperTesting<ClassLoaderResourceProviders> {
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public final class ClassLoaderResourceProvidersTest implements PublicStaticHelperTesting<ClassLoaderResourceProviders>,
+        ClassLoaderResourceProviderTesting {
+
+    // jarFileWithLibs..................................................................................................
+
+    @Test
+    public void testJarFileWithLibsWithNullInputStreamFails() {
+        assertThrows(
+                NullPointerException.class,
+                () -> ClassLoaderResourceProviders.jarFileWithLibs(null)
+        );
+    }
+
+    @Test
+    public void testJarFileWithLibsWithNoLibs() throws IOException {
+        final byte[] resource1 = new byte[]{
+                '1',
+                '1',
+                '1'
+        };
+
+        final byte[] resource2 = new byte[]{
+                '2',
+                '2',
+                '2'
+        };
+
+        final byte[] jar = createJar(
+                Maps.of(
+                        "test/test-resource111.txt",
+                        resource1,
+                        "test/test-resource222.txt",
+                        resource2
+                )
+        );
+
+        final ClassLoaderResourceProvider provider = ClassLoaderResourceProviders.jarFileWithLibs(
+                new JarInputStream(
+                        new ByteArrayInputStream(jar)
+                )
+        );
+
+        this.loadAndCheck(
+                provider,
+                ClassLoaderResourcePath.parse("/test/test-resource111.txt"),
+                ClassLoaderResource.with(
+                        Binary.with(
+                                resource1
+                        )
+                )
+        );
+
+        this.loadAndCheck(
+                provider,
+                ClassLoaderResourcePath.parse("/test/test-resource222.txt"),
+                ClassLoaderResource.with(
+                        Binary.with(
+                                resource2
+                        )
+                )
+        );
+    }
+
+
+    @Test
+    public void testJarFileWithLibsWithLibs() throws IOException {
+        final byte[] resource1 = new byte[]{
+                '1',
+                '1',
+                '1'
+        };
+
+        final byte[] resource2 = new byte[]{
+                '2',
+                '2',
+                '2'
+        };
+
+        final byte[] libs = createJar(
+                Maps.of(
+                        "test/test-resource111.txt",
+                        resource1,
+                        "test/test-resource222.txt",
+                        "ignored".getBytes(StandardCharsets.UTF_8)
+                )
+        );
+
+        final byte[] jar = createJar(
+                Maps.of(
+                        "test/test-resource222.txt",
+                        resource2,
+                        "libs/test.jar",
+                        libs
+                )
+        );
+
+
+        final ClassLoaderResourceProvider provider = ClassLoaderResourceProviders.jarFileWithLibs(
+                new JarInputStream(
+                        new ByteArrayInputStream(jar)
+                )
+        );
+
+        this.loadAndCheck(
+                provider,
+                ClassLoaderResourcePath.parse("/test/test-resource111.txt"),
+                ClassLoaderResource.with(
+                        Binary.with(
+                                resource1
+                        )
+                )
+        );
+
+        this.loadAndCheck(
+                provider,
+                ClassLoaderResourcePath.parse("/test/test-resource222.txt"),
+                ClassLoaderResource.with(
+                        Binary.with(
+                                resource2
+                        )
+                )
+        );
+    }
+
+    private static byte[] createJar(final Map<String, byte[]> contents) throws IOException {
+        try (final ByteArrayOutputStream bytes = new ByteArrayOutputStream()) {
+            final JarOutputStream jarOut = new JarOutputStream(bytes);
+
+            for (final Map.Entry<String, byte[]> mapEntry : contents.entrySet()) {
+                final JarEntry jarEntry = new JarEntry(mapEntry.getKey());
+
+                final byte[] resource = mapEntry.getValue();
+                jarEntry.setSize(resource.length);
+                jarOut.putNextEntry(jarEntry);
+                jarOut.write(resource);
+                jarOut.closeEntry();
+
+            }
+
+            jarOut.flush();
+            jarOut.finish();
+            jarOut.close();
+
+            return bytes.toByteArray();
+        }
+    }
+
+    // ClassFile........................................................................................................
 
     @Override
     public Class<ClassLoaderResourceProviders> type() {
